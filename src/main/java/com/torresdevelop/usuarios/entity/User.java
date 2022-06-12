3d1e1639d78c02dbe33ce74110b1946aeb43071b
Cristian Torres <torreschegue@gmail.com>
2022-06-11 22:24:21 -0500

    Creacion se logico de Service getUserAll, Controlador y Entity

--- src/main/java/com/torresdevelop/usuarios/entity/User.java
+++ src/main/java/com/torresdevelop/usuarios/entity/User.java
@@ -1,5 +1,23 @@
 package com.torresdevelop.usuarios.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
+	
+	private String Id;
+	private String nickName;
+	private String userName;
+	private String password;
+	
 
 }
--- src/main/java/com/torresdevelop/usuarios/service/UserService.java
+++ src/main/java/com/torresdevelop/usuarios/service/UserService.java
@@ -1,5 +1,37 @@
 package com.torresdevelop.usuarios.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.javafaker.Faker;
+import com.torresdevelop.usuarios.entity.User;
+
+@Service
 public class UserService {
 
+	@Autowired
+	private Faker faker;
+	
+	// lista de usuarios
+	private List<User> user = new ArrayList<>();
+	
+	@PostConstruct // Indicar que este metodo tenga peoridad de inicializar cada vez que sea ejecutado
+	public void init() {
+		for(int i = 0; i<10; i++) {
+			user.add((new User(faker.idNumber().invalid(),faker.funnyName().name(), faker.name().username(),faker.dragonBall().character() )));
+		}
+	}
+
+	public List<User> getUser() {
+		return user;
+	}
+	
+	
+	
+	
 }
